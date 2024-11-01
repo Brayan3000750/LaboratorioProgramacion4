b205004093093c665c1fcd2017f079fcb5ee9193
Brayan3000750 <brayanpinel030@gmail.com>
2024-11-01 08:46:03 -0600

    Establecemos la clase como main y estatica de una vez

---
+++ src/Brayan_Pinel_Estructura.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Usuario
+ */
+public class Brayan_Pinel_Estructura {
+    public static void main(String[] args){
+        
+    
+    
+    
+    
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
