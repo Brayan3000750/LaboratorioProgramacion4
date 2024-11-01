0867e686cefd97573b9b6d24368164767a750597
Brayan3000750 <brayanpinel030@gmail.com>
2024-11-01 08:58:54 -0600

    Ponemos el menu con las opciones y despues implementamos lo que es un switch para que se pueda ingresar y ejecutar cada uno de ellos

--- src/Brayan_Pinel_Estructura.java
+++ src/Brayan_Pinel_Estructura.java
@@ -1,3 +1,6 @@
+
+import java.util.Scanner;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
@@ -9,7 +12,38 @@
  */
 public class Brayan_Pinel_Estructura {
     public static void main(String[] args){
+        Scanner datos=new Scanner(System.in);
+    Aleatorio aleatorio=new Aleatorio();
+    int opciones;
+        int cuentaReversa = 0, cuentaPerfecta = 0, cuentaVotos,cuentaPrimas;
+
+   System.out.println("MENU");
+   System.out.println("1. Palabras Alreves");
+    System.out.println("2 - Número Perfecto");
+            System.out.println("3 - Primos");
+            System.out.println("4 - Votaciones");
+            System.out.println("5 - Salir");
+            System.out.print("Seleccione una opción: ");
+            opciones = datos.nextInt();
+            datos.nextLine();
+    
+    switch(opciones){
         
+        case 1:
+            
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+    
+    
+    
+    
     
     
     
