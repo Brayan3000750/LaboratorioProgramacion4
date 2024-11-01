825830f69d2d19d229a612547079e61fb3eaa3f9
Brayan3000750 <brayanpinel030@gmail.com>
2024-11-01 09:35:52 -0600

    En el case 1 y case 2 agregamos los comandos necesarios como if y for para que los programas ejecuten correctamente

--- src/Brayan_Pinel_Estructura.java
+++ src/Brayan_Pinel_Estructura.java
@@ -13,31 +13,63 @@ import java.util.Scanner;
 public class Brayan_Pinel_Estructura {
     public static void main(String[] args){
         Scanner datos=new Scanner(System.in);
-    Aleatorio aleatorio=new Aleatorio();
+    
     int opciones;
-        int cuentaReversa = 0, cuentaPerfecta = 0, cuentaVotos,cuentaPrimas;
+        int cuentaReversa = 0, numeroPerfecto = 0, cuentaVotos=0,cuentaPrimas=0;
 
    System.out.println("MENU");
    System.out.println("1. Palabras Alreves");
-    System.out.println("2 - Número Perfecto");
+    System.out.println("2 - Numero Perfecto");
             System.out.println("3 - Primos");
             System.out.println("4 - Votaciones");
             System.out.println("5 - Salir");
-            System.out.print("Seleccione una opción: ");
+            System.out.print("Seleccione una de mis opciones: ");
             opciones = datos.nextInt();
             datos.nextLine();
     
     switch(opciones){
         
         case 1:
+            cuentaReversa++;
+            System.out.println("Ingrese una palabra");
+        
+        String numero = datos.nextLine();
+                    String reversa = new StringBuilder(numero).reverse().toString();
+                    System.out.println("La palabra alreves es: " + reversa);
+                    break;
+        
+        case 2: 
             
+            numeroPerfecto++;
+            System.out.println("Ingrese un numero");
+            int numeros = datos.nextInt();
+            int sum = 0;
+                    for (int i = 1; i <= numeros / 2; i++) {
+                        if (numeros % i == 0) {
+                            sum += i;
+                        }
+                    }
+                    if(sum==numeros){
+                    System.out.println(numeros + "es un numero perfecto");
+                    
+                    }else {
+                        System.out.println(numeros + " no es perfecto.");
+                    }
+                    break;
         
+        case 3:
+            
+            cuentaPrimas++;
         
-        
-        
-        
-        
-        
+            
+            
+            
+            
+            
+            
+            
+            
+            
     }
     
     
